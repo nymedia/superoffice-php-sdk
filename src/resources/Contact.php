dce3fdacfbbe91f9d16b56d0820f791b91ec7da6
Eirik S. Morland <eirik@morland.no>
2020-11-06 07:50:04 +0100

    Add some contact methods

--- src/resources/Contact.php
+++ src/resources/Contact.php
@@ -9,4 +9,14 @@ class Contact extends RestBase
 
   protected $resourcePath = 'Contact';
 
+  public function getPersonsForContact($id)
+  {
+      return $this->get(sprintf('%s/Persons', $id));
+  }
+
+  public function getDuplicates($name)
+  {
+      return $this->get(sprintf('Duplicates/%s', $name));
+  }
+
 }
